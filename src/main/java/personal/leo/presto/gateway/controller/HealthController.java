e1d4e914e8b7ac2d7c7001c772b972f7001afcbd
leo <leoiscoding@163.com>
2020-11-12 11:57:44 +0800

    增加健康检查

---
+++ src/main/java/personal/leo/presto/gateway/controller/HealthController.java
@@ -0,0 +1,16 @@
+package personal.leo.presto.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("health")
+public class HealthController {
+
+    @GetMapping
+    public String addCoordinator() {
+        return "ok";
+    }
+
+}
